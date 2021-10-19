a8c2db7e218565e44b070076c920e91d4d2392b4
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-10-19 03:37:17 +0200

    refactor(Maximum): add static `isMaximum()` method, add the default value to constructor from the static `Maximum` and update jsdoc.

--- src/lib/maximum.class.ts
+++ src/lib/maximum.class.ts
@@ -3,6 +3,7 @@ import {
   ResultCallback,
   // Function.
   guardNumber,
+  isInstance,
 } from '@angular-package/type';
 /**
  * The `Maximum` primitive wrapper object represents the maximum `number`.
@@ -11,23 +12,23 @@ export class Maximum<Max extends number> extends Number {
   //#region static properties.
   //#region static public properties.
   /**
-   * The static `get` property of `number` returns the primitive value of the `Maximum` instance if set otherwise returns `undefined`.
+   * The static `get` property of `number` type returns the primitive value of the `Maximum` instance if set otherwise returns `undefined`.
    */
   public static get get(): number {
     return this.#maximum?.valueOf();
   }
 
   /**
-   * The static `set` property of `number` type sets a new instance of `Maximum` with its provided value. The set value can be got by the
-   * static `get` and `value` properties, or static `getMaximum()` method.
+   * The static `set` property of the `number` type sets a new instance of `Maximum` with its provided value. The set value can be got by
+   * the static `get` and `value` properties, or the static `getMaximum()` method.
    */
   public static set set(maximum: number) {
     this.value = maximum;
   }
 
   /**
-   * The static `value` property of `number` type sets a new instance of `Maximum` with its provided value and returns the primitive value
-   * of `Maximum` instance if set, otherwise returns `undefined`.
+   * The static `value` property of the `number` type sets a new instance of `Maximum` with its provided value and returns the primitive
+   * value of `Maximum` instance if set, otherwise returns `undefined`.
    */
   public static get value(): number {
     return this.#maximum?.valueOf();
@@ -79,16 +80,30 @@ export class Maximum<Max extends number> extends Number {
   }
 
   /**
+   * The static `isMaximum()` method checks the provided value of any type whether is an instance of `Maximum`.
+   * @param value The value of any type to test against the `Maximum` instance.
+   * @param callback An optional callback function of the `ResultCallback` type to handle the result of the check whether the provided value
+   * is an instance of `Maximum`.
+   * @returns The return value is a `boolean` indicating whether the provided value is an instance of `Maximum`.
+   */
+  public static isMaximum<Max extends number>(
+    value: any,
+    callback?: ResultCallback<any>
+  ): value is Maximum<Max> {
+    return isInstance(value, Maximum, callback);
+  }
+
+  /**
    * The static `setMaximum()` method sets a new instance of `Maximum` with the provided `maximum`.
-   * @param maximum The maximum value of the `number` type to set a new instance.
+   * @param maximum The maximum value of the generic type variable `Max` to set a new instance of `Maximum`.
    * @param callback An optional callback `function` of the `ResultCallback` type to handle the result of the check whether the provided
    * `maximum` is a `number` type.
    * @returns The return value is static `Maximum`.
    * @angularpackage
    */
-  public static setMaximum(
-    maximum: number,
-    callback?: ResultCallback<number>
+  public static setMaximum<Max extends number>(
+    maximum: Max,
+    callback?: ResultCallback<Max>
   ): typeof Maximum {
     guardNumber(maximum, callback) && (this.#maximum = new Maximum(maximum));
     return this;
@@ -98,20 +113,25 @@ export class Maximum<Max extends number> extends Number {
   //#region constructor.
   /**
    * Creates a new instance of `Maximum`.
-   * @param maximum The required immutable maximum of `number` type to set. The value can be picked by property `get` or `valueOf()` method.
+   * @param maximum The immutable maximum value of generic type variable `Max` of the `Maximum` instance being created. Its default
+   * value can be set by the static `set` and `value` properties, or static `setMaximum()` method and can be picked by property `get` or
+   * `valueOf()` method of an `Maximum` instance.
    * @param callback An optional callback function of the `ResultCallback` type to handle the result of the check whether the provided
-   * `maximum` is a number type.
+   * `maximum` is a `number` type.
    * @angularpackage
    */
-  constructor(maximum: Max, callback?: ResultCallback<Max>) {
-    super((guardNumber(maximum, callback) && maximum) || undefined);
+  constructor(
+    maximum: Max = Maximum.#maximum?.get,
+    callback?: ResultCallback<Max>
+  ) {
+    super((guardNumber(maximum, callback) && maximum) || 0);
   }
   //#endregion constructor.
 
   //#region instance public methods.
   /**
    * The `valueOf()` method returns the primitive value of the generic type variable `Max` of the specified `Maximum` object.
-   * @returns The return value is `number` of generic type variable `Max`.
+   * @returns The return value is the `number` of the generic type variable `Max`.
    * @angularpackage
    */
   public valueOf(): Max {
