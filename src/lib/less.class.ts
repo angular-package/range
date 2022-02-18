9ff2cee7bb56548ad874a4f925b2f75101164fae
angularpackage <sciborrudnicki@wvvw.dev>
2022-02-18 15:45:25 +0100

    feat(Less): add feature, the `Less` number object represents the primitive value greater than the given.

---
+++ src/lib/less.class.ts
@@ -0,0 +1,66 @@
+/**
+ * The `Less` number object represents the primitive value greater than the given.
+ */
+export class Less<Value extends number> extends Number {
+  /**
+   * The `get` accessor, with the help of `toStringTag`, changes the default tag to `Less` for an instance of `Less`. It can be read
+   * by the `typeOf()` function of `@angular-package/type`.
+   * @returns The return value is the word 'Less` of a `string`.
+   * @angularpackage
+   */
+  public get [Symbol.toStringTag](): string {
+    return 'Less';
+  }
+
+  /**
+   * Creates the `Less` instance with the given `value`.
+   * @param value The value of generic type variable `Value` to set with a new instance.
+   * @angularpackage
+   */
+  constructor(value: Value) {
+    super(value);
+  }
+
+  /**
+   * Checks whether the primitive value of a specified object is less than the given `value`.
+   * @param value The value of number type to test.
+   * @returns The return value is a `boolean` indicating whether the primitive value is less than the given value.
+   * @angularpackage
+   */
+  public than(value: number): boolean {
+    return typeof value === 'number' ? this.valueOf() < value : false;
+  }
+
+  /**
+   * Checks whether the primitive value of a specified object is less than every given value.
+   * @param values A rest parameter of the numbers to test.
+   * @returns The return value is a `boolean` indicating whether the primitive value is less than every given value.
+   * @angularpackage
+   */
+  public thanEvery(...values: number[]): boolean {
+    return Array.isArray(values)
+      ? values.every((value) => this.valueOf() < value)
+      : false;
+  }
+
+  /**
+   * Checks whether the primitive value of a specified object is less than some given `values`.
+   * @param values A rest parameter of the numbers to test.
+   * @returns The return value is a `boolean` indicating whether the primitive value is less than some given `values`.
+   * @angularpackage
+   */
+  public thanSome(...values: number[]): boolean {
+    return Array.isArray(values)
+      ? values.some((value) => this.valueOf() < value)
+      : false;
+  }
+
+  /**
+   * Returns the primitive value of a specified object.
+   * @returns The return value is the primitive value of generic type variable `Value`.
+   * @angularpackage
+   */
+  public valueOf(): Value {
+    return super.valueOf() as any;
+  }
+}
