a9a5eb3a614e13e2f69e39a8d4c7deb3d9002b0f
angularpackage <sciborrudnicki@wvvw.dev>
2022-02-22 00:17:10 +0100

    refactor(Inequality): add `isBetween()` `isBetweenEvery()` `isBetweenSome()` methods.

--- src/lib/inequality.class.ts
+++ src/lib/inequality.class.ts
@@ -54,6 +54,42 @@ export abstract class Inequality<Value extends number> extends Number {
 
   //#region instance public methods.
   /**
+   * The `isBetween()` method checks whether the primitive value is between the range of a specified object.
+   * @param min The minimum range of number type to test.
+   * @param max The maximum range of number type to test.
+   * @returns The return value is a `boolean` type indicating whether the primitive value is between the range of a specified object.
+   * @angularpackage
+   */
+  public isBetween(min: number, max: number): boolean {
+    return min < max
+      ? (this.greaterThan(min) && this.lessThan(max)) ||
+          min === this.valueOf() ||
+          max === this.valueOf()
+      : false;
+  }
+
+  /**
+   * Checks whether the primitive value is between every range of the given `ranges`.
+   * @param ranges A rest parameter of `array` type ranges to test.
+   * @returns The return value is a `boolean` type indicating whether the primitive value is between every range of the
+   * given `ranges`.
+   * @angularpackage
+   */
+  public isBetweenEvery(...ranges: [number, number][]): boolean {
+    return ranges.every((range) => this.isBetween(range[0], range[1]));
+  }
+
+  /**
+   * Checks whether the primitive value is between some given `ranges`.
+   * @param ranges A rest parameter of `array` type ranges to test.
+   * @returns The return value is a `boolean` type indicating whether the primitive value is between some given `ranges`.
+   * @angularpackage
+   */
+  public isBetweenSome(...ranges: [number, number][]): boolean {
+    return ranges.some((range) => this.isBetween(range[0], range[1]));
+  }
+
+  /**
    * Checks whether the primitive value of a child class instance is greater than the given `value`.
    * @param value The value of `number` type to test.
    * @returns The return value is a `boolean` indicating whether the primitive value of a child class instance is greater than the given
